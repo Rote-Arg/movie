d2036c56a80984efebfbcb3b1ee11d70d7257f99
Rote-Arg <elrote@gmail.com>
2021-10-02 17:06:52 -0300

    la pc esta lenta y vamos a dejarla descansar

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './Home.css';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+export default function Home() {
+
+    const { Dragger } = Upload;
+
+    const props = {
+        name: 'file',
+        multiple: true,
+        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+        onChange(info) {
+        const { status } = info.file;
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+        },
+        onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+        },
+    };
+
+    return(
+        <div>
+            <h1>¿Quien es este actor?</h1>
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">
+                    Click or drag file to this area to upload
+                </p>
+                <p className="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                    band files
+                </p>
+            </Dragger>
+        </div>
+    );
+}
