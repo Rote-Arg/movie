42cf623be729793614d2da3b315b2f3a7c7663bb
Rote-Arg <elrote@gmail.com>
2021-10-03 12:18:54 -0300

    avanzamos  con las request y como enviar la imagen

--- api/src/rutas.js
+++ api/src/rutas.js
@@ -1,8 +1,16 @@
+const { default: axios } = require('axios');
 const {Router} = require('express');
 const router = Router();
-const axios = require('axios');
+const server = require("express").Router();
+require('dotenv').config();
+const {NOMADA_KEY, MOVIE_KEY} = process.env;
 
+console.log(NOMADA_KEY, MOVIE_KEY)
 
-
+/* server.post('/' , (req, res, next) => {
+    console.log(req)
+    .then( res.status(200).send('Salio peola, bro') )
+    .catch( next() )
+}) */
 
 module.exports = router
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'antd/dist/antd.css';
 import './Home.css';
 import { Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
-//import axios from 'axios';
+import axios from 'axios';
+
 
 export default function Home() {
 
-    const { Dragger } = Upload;
+    var NOMADA_KEY = 'ZTdkNDFkMGYtMWZkNi00ZWU5LThkYTgtYTRlN2UzODNjZGE1';
+    var MOVIE_KEY= '30db1237b9167f8afaf9e065b90d16b8';
 
+
+    //const [image, setImage] = useState('')
+
+    const { Dragger } = Upload;
+    
     const props = {
-        name: 'imageToSearch',
+        name: 'file',
         multiple: false,
         maxCount: 1,
         accept: '.png, .PNG, .jpg, .JPG',
-        action: 'https://whois.nomada.cloud/upload',
+        action: null,
         method: 'POST',
-        key: '',
+        file: '',
         onChange(info) {
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
             if (status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully.`);
+                //message.success(`${info.file.name} file uploaded successfully.`)
+                console.log("Linea 31", info.file.uid)
             } else if (status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
+                //message.error(`${info.file.name} file upload failed.`);
+                console.log("Linea 34", info.file.uid)
+                axios.post( `https://whois.nomada.cloud/upload?nomada=${NOMADA_KEY}&file=${info.file.uid}`)
+                    .then(res => console.log(res))
+                    .catch(err => console.log(err))
             }
         },
         
